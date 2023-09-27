6ac4cba59c7c4ba8dc6647017de037a3d6d5aadf
rajmaurya95 <gautamshakya9528@gmail.com>
2023-09-27 12:05:13 +0530

    Finally uplaoded on git hub

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -18,7 +18,7 @@ const Navbar = () => {
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0" style={{}}>
                         <li className="nav-item">
-                            <Link  className={`nav-link ${location.pathname==='/'?'active':''}`} aria-current="page" to="/">Home</Link>
+                            <Link  className={`nav-link ${location.pathname==='/'?'active':''}`} aria-current="page" to="/my_website">Home</Link>
                         </li>
                         <li className="nav-item">
                             <Link  className={`nav-link ${location.pathname==='/about'?'active':''}`} to="/about">About</Link>
